eecfe74c6fc571927ae2872458fdd3db16b4c752
helloWorld <peoplenamed@gmail.com>
2023-11-23 20:55:04 -0500

    Added more faces

--- JpegViewer-ESP8266-GA9C01.ino
+++ JpegViewer-ESP8266-GA9C01.ino
@@ -35,7 +35,7 @@ void setup()
     #ifdef DEBUG
       Serial.println("gfx->begin() initialized.");
     #endif
-    gfx->fillScreen(BLACK);
+    wipeScreen(true);
   }
   if (!LittleFS.begin())
   {
@@ -59,14 +59,38 @@ void loop()
 
 void runCommand() {
   switch (imageSelect) {
-    case 1:
+    case 0:
       octocat();
       break;
-    case 2:
+    case 1:
       calvinAndHobbes();
       break;
-    case 3:
+    case 2:
       grumpyFace();
+      break;
+    case 3:
+      winkFace();
+      break;
+    case 4:
+      disappointedFace();
+      break;
+    case 5:
+      satisfiedFace();
+      break;
+    case 6:
+      normalFace();
+      break;
+    case 20:
+      circleWipe(10, false);
+      break;
+    case 21:
+      circleWipe(10, true);
+      break;
+    case 22:
+      triangleWipe(true);
+      break;
+    case 23:
+      triangleWipe(false);
       break;
     default:
       calvinDuplicator();
@@ -74,11 +98,25 @@ void runCommand() {
   }
 }
 
+/** STATIC IMAGES **/
 void calvinAndHobbes(){ drawImage("/download.jpeg"); }
 void octocat(){ drawImage("/octocat.jpg"); }
-void grumpyFace() { drawJpgAnimation("grumpy_face_0", ".jpg", 4, 50); }
+
+/** ANIMATIONS **/
 void calvinDuplicator() { drawJpgAnimation("1Ys_", ".jpg", 19, 3); }
 
+/** FACES **/
+void grumpyFace() { drawJpgAnimation("grumpy_face_0", ".jpg", 4, 20); }
+void winkFace() { drawJpgAnimation("wink_face_", ".jpg", 5, 1); }
+void disappointedFace() { drawJpgAnimation("disappointed_face_", ".jpg", 5, 1); }
+void normalFace() { drawImage("normal_face"); }
+void satisfiedFace() {
+  normalFace();
+  delay(400);
+  drawImage("satisfied_face");
+  delay(400);
+  normalFace();
+}
 
 void drawJpgAnimation(String name, String fileType, int frames, int times) {
   for (int x=1; x <= times; x++) {
@@ -100,6 +138,55 @@ void drawJpgAnimation(String name, String fileType, int frames, int times) {
   
       drawImage(charArray);
     }
+  }
+}
+
+void circleWipe(int radius, boolean wipe) {
+  wipeScreen(wipe);
+  int32_t w = _width;
+  int32_t h = _height;
+  uint32_t start;
+  int32_t x, y;
+  int32_t r2 = radius * 2;
+  int32_t w1 = w + radius;
+  int32_t h1 = h + radius;
+
+  for (x = 0; x < w1; x += r2) {
+    for (y = 0; y < h1; y += r2) {
+      gfx->drawCircle(x, y, radius, WHITE);
+    }
+  }
+}
+
+void wipeScreen(boolean wipe) {
+  if (wipe) {
+    gfx->fillScreen(BLACK);
+  }
+}
+
+void triangleWipe(boolean wipe) {
+  wipeScreen(wipe);
+  int32_t i;
+//  int32_t n = min(w, h);
+//  int32_t n1 = n - 1;
+  int32_t cx = _width / 2;
+  int32_t cy = _height / 2;
+  int32_t cx1 = cx - 1;
+  int32_t cy1 = cy - 1;
+  int32_t cn = min(cx1, cy1);
+//  int32_t cn1 = cn - 1;
+//  int32_t tsa = ((w <= 176) || (h <= 160)) ? 1 : (((w <= 240) || (h <= 240)) ? 2 : 3); // text size A
+//  int32_t tsb = ((w <= 272) || (h <= 220)) ? 1 : 2;                                    // text size B
+//  int32_t tsc = ((w <= 220) || (h <= 220)) ? 1 : 2;                                    // text size C
+//  int32_t ds = (w <= 160) ? 9 : 12;                                                    // digit size
+
+
+  for (i = 0; i < cn; i += 5) {
+    gfx->drawTriangle(
+        cx1, cy1 - i,     // peak
+        cx1 - i, cy1 + i, // bottom left
+        cx1 + i, cy1 + i, // bottom right
+        gfx->color565(0, 0, i));
   }
 }
 
